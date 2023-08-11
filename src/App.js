fed2680cf0a17804d21911123ba50cd87c7a14d5
EFP18 <ester.p18@hotmail.com>
2023-08-11 13:23:30 -0500

    experience, icons for technologies, education added

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
       <HashRouter>
         <Navigation />
         <About />
-        {/* <Experience/> */}
+        <Experience />
         <Projects />
         <Footer />
       </HashRouter>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,54 +1,91 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import headshot from './images/headshotPortfolio.png';
+import mysqlIcon from './images/icons8-mysql-96.png';
+import javascriptIcon from './images/icons8-javascript-96.png';
+import nodeIcon from './images/icons8-nodejs-96.png';
+import reactIcon from './images/icons8-react-native-96.png';
+import cssIcon from './images/icons8-css-96.png';
+import expressIcon from './images/icons8-express-js-96.png';
+import handlebarIcon from './images/icons8-handlebar-96.png';
+import htmlIcon from './images/icons8-html-96.png';
+import mongoIcon from './images/icons8-mongodb-96.png';
+import resume from './images/resume.pdf';
 
-const About = () => {
+const Experience = () => {
   return (
     <Container>
-      {/* create a row in the page that will have 2 columns, one for the headshot and one for the bio */}
-      <Row>
-        <img
-          src={headshot}
-          className='rounded-circle headshot'
-          alt='headshot'
-          style={{
-            height: '150px',
-            marginLeft: '50px',
-            width: '130px',
-          }}
-        />
-        <Col sm={10}>
-          <div className='mb-3 ml-4 position-fixed'>
-            <div className='card-body'>
-              <h5 className='card-title cardTitle mb-4'>
-                Hey! My name is Ester!
-              </h5>
-              <p className='card-text'>
-                Very methodical and hard working, I enjoy solving puzzles and
-                answering complex questions. I am very passionate about
-                languages and I'm very interested in learning some programming
-                languages as well, to combine them with creativity. I am very
-                eager to learn, well-organized, and I work very well with
-                deadlines.
-              </p>
-              <p className='card-text'>
-                I studied Linguistics at the University of Groningen in the
-                Netherlands. I'm really passionate about studying different
-                languages, and I find the creation of different structures and
-                sentences very interesting, which is why I decided to study
-                programming languages and coding.
-              </p>
-              <p className='card-text'>
-                <small className='text-muted'>
-                  Contact me for more information!!
-                </small>
-              </p>
-            </div>
+      <Row style={{ marginBottom: '50px' }}>
+        <Col xs={12} md={6} lg={6}>
+          <h1 style={{ marginBottom: '30px' }}>Skills</h1>
+          <div style={{ marginLeft: 'auto' }}>
+            <img
+              src={mysqlIcon}
+              alt='mysqlIcon'
+              style={{ marginRight: '30px' }}
+            />
+            <img
+              src={javascriptIcon}
+              alt='javascriptIcon'
+              style={{ marginRight: '30px' }}
+            />
+            <img
+              src={nodeIcon}
+              alt='nodeIcon'
+              style={{ marginRight: '30px' }}
+            />
+            <img
+              src={reactIcon}
+              alt='reactIcon'
+              style={{ marginRight: '30px' }}
+            />
+            <img
+              src={expressIcon}
+              alt='expressIcon'
+              style={{ marginRight: '30px' }}
+            />
           </div>
+          <div style={{ marginLeft: 'auto' }}>
+            <img src={cssIcon} alt='cssIcon' style={{ marginRight: '30px' }} />
+            <img
+              src={handlebarIcon}
+              alt='handlebarIcon'
+              style={{ marginRight: '30px' }}
+            />
+            <img
+              src={htmlIcon}
+              alt='htmlIcon'
+              style={{ marginRight: '30px' }}
+            />
+            <img
+              src={mongoIcon}
+              alt='mongoIcon'
+              style={{ marginRight: '30px' }}
+            />
+          </div>
+        </Col>
+        <Col>
+          <h1 style={{ marginBottom: '30px' }}>Education</h1>
+          <div>
+            <h3>Full Stack Web Development</h3>
+            <p>Vanderbilt, 2023</p>
+          </div>
+          <div>
+            <h3>European Languages and Cultures</h3>
+            <p>University of Groningen, 2017</p>
+          </div>
+          <a
+            href={resume}
+            target='_blank'
+            rel='noreferrer'
+            className='btn'
+            style={{ width: 'auto', color: '#0FF0FC' }}
+          >
+            Click here to view my full resume!
+          </a>
         </Col>
       </Row>
     </Container>
   );
 };
 
-export default About;
+export default Experience;
