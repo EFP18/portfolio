bf7a4e5e472597dc6794b5286293e86f7ebf0c30
EFP18 <ester.p18@hotmail.com>
2023-08-14 09:38:40 -0500

    navbar hamburger close on click

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Navigation.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleNavClickToggle = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <Navbar expand='lg' className='bg-body-tertiary sticky-top'>
+    <Navbar
+      expand='lg'
+      className='bg-body-tertiary sticky-top'
+      expanded={isOpen}
+    >
       <Container>
         <Navbar.Brand href='#home' className='name'>
           Ester Pelosof
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Toggle
+          aria-controls='basic-navbar-nav'
+          onClick={() => setIsOpen(!isOpen)}
+        />
         <Navbar.Collapse id='basic-navbar-nav' style={{ flexGrow: '0' }}>
           <Nav className='me-auto navItems' style={{ fontSize: '30px' }}>
-            <Nav.Link href='#about'>About</Nav.Link>
-            <Nav.Link href='#experience'>Experience</Nav.Link>
-            <Nav.Link href='#projects'>Projects</Nav.Link>
+            <Nav.Link href='#about' onClick={handleNavClickToggle}>
+              About
+            </Nav.Link>
+            <Nav.Link href='#experience' onClick={handleNavClickToggle}>
+              Experience
+            </Nav.Link>
+            <Nav.Link href='#projects' onClick={handleNavClickToggle}>
+              Projects
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
